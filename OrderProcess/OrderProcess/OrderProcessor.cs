d88aef3af1b2a4746e765848bfb2c474efff556b
nazrinmajidova <majidovanazrin@gmail.com>
2024-08-19 23:55:22 +0400

    InitialCommit

---
+++ OrderProcess/OrderProcess/OrderProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using OrderProcess.Models;
+
+namespace OrderProcess;
+
+public class OrderProcessor
+{
+    private readonly ILogger<OrderProcessor> _logger;
+
+    public OrderProcessor(ILogger<OrderProcessor> logger)
+    {
+        _logger = logger;
+    }
+
+    public void ProcessOrder(Order order)
+    {
+        _logger.LogInformation("Starting to process order {OrderId} for {CustomerName}", order.Id, order.CustomerName);
+        try
+        {
+            if (order.Amount <= 0)
+            {
+                throw new InvalidOperationException("Order amount should be more than zero.");
+            }
+
+            // Simulate order processing
+            order.IsProcessed = true;
+            _logger.LogInformation("Order {OrderId} processed successfully.", order.Id);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error occured when processing order {OrderId}", order.Id);
+        }
+    }
+}
---
+++ OrderProcess/OrderProcess/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OrderProcess.Models;
+using OrderProcess;
+
+
+ // Setup Dependency Injection
+ var serviceProvider = new ServiceCollection()
+     .AddLogging(config =>
+     {
+         config.AddConsole();
+         config.SetMinimumLevel(LogLevel.Information);
+     })
+     .AddTransient<OrderProcessor>()
+     .BuildServiceProvider();
+
+ // Create a logger
+ var logger = serviceProvider.GetService<ILogger<Program>>();
+
+ logger.LogInformation("Application started.");
+
+
+ var orders = new List<Order>
+ {
+     new Order { Id = 1, CustomerName = "Leyla", Amount = 50 },
+     new Order { Id = 2, CustomerName = "Ehmed", Amount = 0 }, // Wrong order
+     new Order { Id = 3, CustomerName = "Cavidan", Amount = 150 }
+ };
+
+
+ var orderProcessor = serviceProvider.GetService<OrderProcessor>();
+ foreach (var order in orders)
+ {
+     orderProcessor.ProcessOrder(order);
+ }
+
+ logger.LogInformation("Application finished.");
